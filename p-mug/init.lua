6106bbe1002c8e622e9e167e1462ce9e2179e5d0
RamiLego4Game <ramilego4game@gmail.com>
2016-04-22 10:13:11 +0300

    Building the base

---
+++ p-mug/init.lua
@@ -0,0 +1,34 @@
+--P-MUG By: RamiLego4Game--
+local PMug, Path = {}, ...
+
+PMug.ViewsManager = require(Path..".viewsmanager",Path)
+
+function PMug.update(dt)
+  
+end
+
+function PMug.draw()
+  
+end
+
+function PMug.mousepressed(x,y,button,istouch)
+  
+end
+
+function PMug.mousereleased(x,y,button,istouch)
+  
+end
+
+function PMug.keypressed(key,isrepeat)
+  
+end
+
+function PMug.keyreleased(key)
+  
+end
+
+function PMug.textinput(text)
+  
+end
+
+return PMug
---
+++ p-mug/viewsmanager.lua
@@ -0,0 +1,5 @@
+local ViewsManager, Path = {}, ...
+
+
+
+return ViewsManager
