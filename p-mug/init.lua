9c1d0f78b981de9fb3823b717de29092dac6540e
RamiLego4Game <ramilego4game@gmail.com>
2016-04-24 08:55:30 +0300

    Combined ViewsManager in PMug init

--- p-mug/init.lua
+++ p-mug/init.lua
@@ -1,38 +1,94 @@
 --P-MUG By: RamiLego4Game--
-local PMug, Path = {}, ...
+local PMug, Path = {Views={}}, ...
 
-PMug.ViewsManager = require(Path..".viewsmanager",Path)
+local ViewBase = require(Path..".api.viewbase")
 
-function PMug.update(dt)
-  
+function PMug.newView(name,...)
+  local newView = ViewBase(name or "none",...)
+  PMug.registerView(newView)
+  return newView
 end
 
-function PMug.draw()
-  
+function PMug.registerView(view)
+  if not view.static.PMUGV then return error("This isn't a P-Mug view class") end
+  PMug.Views[view:getName()] = view
 end
 
-function PMug.mousepressed(x,y,button,istouch)
-  
+function PMug.setActiveView(name,...)
+  if not PMug.Views[name] then return error("View ["..tostring(name).."] is not registered.") end
+  if PMug.activeView then
+    PMug.activeView:leave(PMug.Views[name],...)
+    PMug.Views[name]:enter(PMug.activeView,...)
+  else
+    PMug.Views[name]:enter(nil,...)
+  end
+  PMug.activeView = PMug.Views[name]
 end
 
-function PMug.mousemoved(x,y,dx,dy)
-
+function PMug.draw(...)
+  if PMug.activeView and PMug.activeView.draw then
+    PMug.activeView:draw(...)
+  end
 end
 
-function PMug.mousereleased(x,y,button,istouch)
-  
+function PMug.update(...)
+  if PMug.activeView and PMug.activeView.update then
+    PMug.activeView:update(...)
+  end
 end
 
-function PMug.keypressed(key,scancode,isrepeat)
-  
+function PMug.keypressed(...)
+  if PMug.activeView and PMug.activeView.keypressed then
+    PMug.activeView:keypressed(...)
+  end
 end
 
-function PMug.keyreleased(key,scancode)
-  
+function PMug.keyreleased(...)
+  if PMug.activeView and PMug.activeView.keyreleased then
+    PMug.activeView:keyreleased(...)
+  end
 end
 
-function PMug.textinput(text)
-  
+function PMug.textinput(...)
+  if PMug.activeView and PMug.activeView.textinput then
+    PMug.activeView:textinput(...)
+  end
+end
+
+function PMug.mousepressed(...)
+  if PMug.activeView and PMug.activeView.mousepressed then
+    PMug.activeView:mousepressed(...)
+  end
+end
+
+function PMug.mousemoved(...)
+  if PMug.activeView and PMug.activeView.mousemoved then
+    PMug.activeView:mousemoved(...)
+  end
+end
+
+function PMug.mousereleased(...)
+  if PMug.activeView and PMug.activeView.mousereleased then
+    PMug.activeView:mousereleased(...)
+  end
+end
+
+function PMug.touchpressed(...)
+  if PMug.activeView and PMug.activeView.touchpressed then
+    PMug.activeView:touchpressed(...)
+  end
+end
+
+function PMug.touchmoved(...)
+  if PMug.activeView and PMug.activeView.touchmoved then
+    PMug.activeView:touchmoved(...)
+  end
+end
+
+function PMug.touchreleased(...)
+  if PMug.activeView and PMug.activeView.touchreleased then
+    PMug.activeView:touchreleased(...)
+  end
 end
 
 return PMug
