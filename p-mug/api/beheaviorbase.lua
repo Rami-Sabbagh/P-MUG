72cf71e59e3773a06de1eb31dc603ad80c529e70
Rami Sabbagh <ramilego4game@gmail.com>
2016-06-06 15:25:23 +0300

    Replaced with handerbase & actionbase

--- p-mug/api/beheaviorbase.lua
+++
@@ -1,6 +0,0 @@
-local Path = string.sub(..., 1, string.len(...) - string.len(".api.beheaviorbase"))
-local Class = require(Path..".third-party.middleclass")
-
-local BBase = Class("beheavior.base") BBase.PMUGB = true --P-Mug Beheavior Class
-
-return BBase
