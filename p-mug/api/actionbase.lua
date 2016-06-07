2f071903308cc5fc9bde952cfdd81d142e29c912
Rami Sabbagh <ramilego4game@gmail.com>
2016-06-07 15:38:03 +0300

    WIP

--- p-mug/api/actionbase.lua
+++ p-mug/api/actionbase.lua
@@ -3,4 +3,32 @@ local Class = require(Path..".third-party.middleclass")
 
 local ABase = Class("action.base") ABase.PMUGA = true --P-Mug Action Class
 
+function ABase:initialize()
+  
+end
+
+function ABase:update(dt)
+  
+end
+
+function ABase:handlerHovered()
+  
+end
+
+function ABase:handlerDown()
+
+end
+
+function ABase:handlerDragged()
+  
+end
+
+function ABase:handlerReleased()
+  
+end
+
+function ABase:handlerCanceled()
+  
+end
+
 return ABase
