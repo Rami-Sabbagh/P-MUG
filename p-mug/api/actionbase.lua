46dc62807c845fe929fbf342bf610dd987f7e6ce
Rami Sabbagh <ramilego4game@gmail.com>
2016-06-07 16:29:39 +0300

    WIP

--- p-mug/api/actionbase.lua
+++ p-mug/api/actionbase.lua
@@ -4,30 +4,34 @@ local Class = require(Path..".third-party.middleclass")
 local ABase = Class("action.base") ABase.PMUGA = true --P-Mug Action Class
 
 function ABase:initialize()
-  
+    self.hd = {} --Handlerdata, Table used by the handler to handle some stuff.
 end
 
 function ABase:update(dt)
   
 end
 
-function ABase:handlerHovered()
+function ABase:handlerHovering(x,y)
   
 end
 
-function ABase:handlerDown()
-
-end
-
-function ABase:handlerDragged()
+function ABase:handlerUnhovered(x,y)
   
 end
 
-function ABase:handlerReleased()
+function ABase:handlerDown(x,y,pressure)
   
 end
 
-function ABase:handlerCanceled()
+function ABase:handlerDragged(x,y,pressure)
+  
+end
+
+function ABase:handlerReleased(x,y,pressure)
+  
+end
+
+function ABase:handlerCancelled(x,y,pressure)
   
 end
 
--- p-mug/api/handlerbase.lua
+++ p-mug/api/handlerbase.lua
@@ -4,7 +4,14 @@ local Class = require(Path..".third-party.middleclass")
 local HBase = Class("handler.base") HBase.PMUGH = true --P-Mug Handler Class
 
 function HBase:initialize()
+  self.Actions = {}
   self.LShapes = {}
+end
+
+function HBase:addAction(act)
+  if not act.PMUGA then return error("This isn't a P-Mug action class") end
+  table.insert(self.Actions,act)
+  return self, #self.Actions
 end
 
 function HBase:linkShape(shp)
@@ -14,28 +21,48 @@ function HBase:linkShape(shp)
 end
 
 function HBase:update(dt)
-  
+  for k, Act in ipairs(self.Actions) do
+    if Act and Act.update then Act:update(dt) end
+  end
 end
 
-function HBase:mousepressed(x,y,button)
+function HBase:mousepressed(x,y,button,istouch)
+  if istouch then return end
   for k,shape in ipairs(self.LShapes) do
     if shape:testShape(x,y) then
-      for k,shp in ipairs(self.LShapes) do
+      --[[for k,shp in ipairs(self.LShapes) do
         shp:isDown(true,x,y)
+      end]]
+      for k, Act in ipairs(self.Actions) do
+        if Act and Act.handlerDown then Act:handlerDown(x,y,button) Act.hd.mp = true end
       end
       break
     end
   end
 end
 
-function HBase:mousereleased(x,y,button)
-  for k,shape in ipairs(self.LShapes) do
-    shape:isDown(false,x,y)
-  end
-end
-
 function HBase:mousemove(x,y,dx,dy)
   
 end
+
+function HBase:mousereleased(x,y,button,istouch)
+  if istouch then return end
+  for k,shape in ipairs(self.LShapes) do
+    if shape:testShape(x,y) then
+      for k, Act in ipairs(self.Actions) do
+        if Act and Act.handlerReleased and Act.hd.mp then Act:handlerReleased(x,y,button) Act.hd.mp = false end
+      end
+      return
+    end
+  end
+  for k, Act in ipairs(self.Actions) do
+    if Act and Act.handlerReleased and Act.hd.mp then Act:handlerCancelled(x,y,button) Act.hd.mp = false end
+  end
+  --[[for k,shape in ipairs(self.LShapes) do
+    shape:isDown(false,x,y)
+  end]]
+end
+
+
 
 return HBase
