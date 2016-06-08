95f6c06547533cd741be8a412ee7d02c771cd9b5
Rami Sabbagh <ramilego4game@gmail.com>
2016-06-08 21:09:43 +0300

    For the frame object

---
+++ p-mug/actions/dragObject.lua
@@ -0,0 +1,12 @@
+local Path = string.sub(..., 1, -string.len(".actions.dragObject"))
+local Class = require(Path..".third-party.middleclass")
+local ABase = require(Path..".api.actionbase")
+
+local ADO = Class("action.onClick",ABase)
+
+function ADO:handlerDragged(x,y,dx,dy,obj,shape,shapes)
+  local oldx, oldy = obj:getPosition()
+  obj:setPosition(oldx+dx, oldy+dy)
+end
+
+return ADO
