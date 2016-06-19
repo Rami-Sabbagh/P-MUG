dabd3e64085b22148422d8c46b97e951e47c8045
Rami Sabbagh <ramilego4game@gmail.com>
2016-06-19 16:21:49 +0300

    A button that creates frames

--- main.lua
+++ main.lua
@@ -1,19 +1,42 @@
-io.stdout:setvbuf("no")
+--e "io.stdout:setvbuf('no'); if os.getenv('DEBUG_MODE') then require 'debugger' ; require 'debugger.plugins.ffi'end"
+io.stdout:setvbuf('no'); --if os.getenv('DEBUG_MODE') then require 'debugger' ; require 'debugger.plugins.ffi'end
+if os.getenv('DEBUG_MODE') then require('debugger')() ; end--require('debugger.plugins.ffi') end
+--io.stdout:setvbuf("no")
 
 local PMug = require("p-mug")
 local OBase = require("p-mug.api.objectbase")
 local HBase = require("p-mug.api.handlerbase")
 
-
 local Material = require("p-mug.third-party.material-love")
 
 function love.load(args)
+  --if args[2] and args[2] == "-e" then loadstring(args[3])() end
   love.graphics.setBackgroundColor(Material.colors.background("light"))
   local TView = PMug.newView("test")
-  local TButton = PMug.Object.button(10,10,100,35,"Quit",function() love.event.quit() end)
-  local TFrame = PMug.Object.frame(100,100,200,200,"Frame Test")
-  TView:registerObject(TButton,0):registerObject(TFrame,4)
+  local TBExit = PMug.Object.button(10,10,100,35,"Quit",function() love.event.quit() end)
+  local TButton = PMug.Object.button(10,55,100,35,"New Frame",function()
+    local TFrame = PMug.Object.frame(100,100,200,200,"Frame Test")
+    TView:registerObject(TFrame,4)
+  end)
+  TView:registerObject(TBExit,0):registerObject(TButton,0)
   PMug.setActiveView("test")
+  generateColorPallete()
+end
+
+function toColor(num)
+  if num < 100 then return " "..tostring(num) elseif num < 10 then return "  "..tostring(num) else return tostring(num) end
+end
+
+function generateColorPallete()
+  local data = "GIMP Palette\nName: Material Auto\nColumns: 21\n#\n"
+  local colors = {"red","pink","purple","deep-purple","indigo","blue","light-blue","cyan","teal","green","light-green","lime","yellow","amber","orange","deep-orange","brown","grey","blue-grey"}
+  local maincolors = {"50","100","200","300","400","500","600","700","800","900","a100","a200","a400","a700"}
+  local bwcolors = {"full","divider","disabled","inactive-icon","hint-text","secondary-text","icon","text","display","headline","title","subhead","body","caption","menu","button"}
+  for k,cl in ipairs(colors) do
+    local r, g, b = Material.colors.main(cl)
+    r, g, b = toColor(r), toColor(g), toColor(b)
+    data = data..r.." "..g.." "..b.."    "..cl.." main"
+  end
 end
 
 function love.update(dt)
