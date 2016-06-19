388cced2505ca699390f1fef43e3054cc38b9798
Rami Sabbagh <ramilego4game@gmail.com>
2016-06-19 16:22:31 +0300

    Bugfix

--- p-mug/actions/dragObject.lua
+++ p-mug/actions/dragObject.lua
@@ -4,10 +4,23 @@ local ABase = require(Path..".api.actionbase")
 
 local ADO = Class("action.onClick",ABase)
 
+function ADO:initialize()
+  ABase.initialize(self)
+  self.obstructed = {}
+end
+
+function ADO:handlerDown(x,y,pressure,obj,shape,shapes,obstruct)
+  if obstruct then self.obstructed[obj] = true elseif self.obstructed[obj] then self.obstructed[obj] = nil end
+end
+
 function ADO:handlerDragged(x,y,dx,dy,obj,shape,shapes,obstruct)
-  if obstruct then return end
+  if self.obstructed[obj] then return end
   local oldx, oldy = obj:getPosition()
   obj:setPosition(oldx+dx, oldy+dy)
+end
+
+function ABase:handlerReleased(x,y,pressure,obj,shape,shapes,obstruct)
+  if self.obstructed[obj] then self.obstructed[obj] = nil end
 end
 
 return ADO
