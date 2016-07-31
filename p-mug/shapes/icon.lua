e76b9a4d2602c3677da55dadf7eaede81d4cd3d2
Rami Sabbagh <ramilego4game@gmail.com>
2016-07-31 21:52:01 +0300

    Bugfix

--- p-mug/shapes/icon.lua
+++ p-mug/shapes/icon.lua
@@ -1,4 +1,4 @@
-local Path = string.sub(..., 1, -string.len(".shapes.text"))
+local Path = string.sub(..., 1, -string.len(".shapes.icon"))
 local Class = require(Path..".third-party.middleclass")
 local SBase = require(Path..".api.shapebase")
 
