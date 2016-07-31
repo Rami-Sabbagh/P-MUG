42b51f52430c8e01af1d522ce745dc3f62cf2b95
Rami Sabbagh <ramilego4game@gmail.com>
2016-07-31 20:24:41 +0300

    New FAB Object

--- p-mug/objects/Material/FAB.lua
+++ p-mug/objects/Material/FAB.lua
@@ -6,6 +6,7 @@ local Material = require(Path..".third-party.material-love")
 
 local OFAB = Class("object.fab",OBase)
 
+function OFAB:initialize(x,y,iconName,colorName,r,oc)
   OBase.initialize(self)
   local PMug = require(Path)
 
@@ -14,6 +15,8 @@ local OFAB = Class("object.fab",OBase)
   self.updateState = PMug.Action.updateState()
 
   --Shapes--
+  self.SBody = PMug.Shape.circle(0,0,r or 28):setDrawingArgs(false,false,{Material.colors(colorName or "red","600")},{Material.colors.main(colorName or "red")},{Material.colors(colorName or "red","400")})
+  self.SIcon = PMug.Shape.icon(iconName or "blinds",0,0,0,"white",true)
 
   --Drawers--
   self.Drawer = PMug.Drawer.material()
@@ -23,13 +26,17 @@ local OFAB = Class("object.fab",OBase)
 
   --Registering--
   self:addDrawer(self.Drawer):addHandler(self.Handler)
+  self:registerShape(self.SBody):registerShape(self.SIcon)
 
+  self.Drawer:linkShape(self.SBody):linkShape(self.SIcon)
 
+  self.Handler:linkShape(self.SBody):linkShape(self.SIcon):addAction(self.updateState):addAction(self.onClick)
 
   self:setPosition(x,y)
 end
 
 function OFAB:getType()
+  return "FAB"
 end
 
 function OFAB:setOnClick(oc)
