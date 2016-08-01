8324ec4817a76390dbb33eb47630741ebf5ab96e
Rami Sabbagh <ramilego4game@gmail.com>
2016-08-01 11:44:28 +0300

    Changed the color of the circle

--- p-mug/objects/Material/Slider.lua
+++ p-mug/objects/Material/Slider.lua
@@ -11,10 +11,11 @@ function OSlider:initialize(x,y,length,orientation,min,max)
   local PMug = require(Path)
 
   --Shapes--
-  self.SCircle = PMug.Shape.circle(0,0,10):setDrawingArgs(true,true,{Material.colors("red","600")},{Material.colors.main("red")},{Material.colors("red","400")})
+  self.SCircle = PMug.Shape.circle(0,0,10):setDrawingArgs(true,true,{0,0,0,0},{Material.colors.main("blue-grey")},{Material.colors.main("light-blue")})
 
   --Actions--
   self.slideAction = PMug.Action.slideShape(0,0,length,orientation,self.SCircle)
+  self.updateStateAction = PMug.Action.updateState()
 
   --Drawers--
   self.Drawer = PMug.Drawer.material()
@@ -25,7 +26,7 @@ function OSlider:initialize(x,y,length,orientation,min,max)
   --Registering--
   self:addHandler(self.Handler):addDrawer(self.Drawer)
 
-  self.Handler:linkShape(self.SCircle):addAction(self.slideAction)
+  self.Handler:linkShape(self.SCircle):addAction(self.slideAction):addAction(self.updateStateAction)
   self.Drawer:linkShape(self.SCircle)
 
   self:registerShape(self.SCircle)
