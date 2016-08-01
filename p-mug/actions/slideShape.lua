ffdc8179af82727cd879dbd5b11c2bd04b89a119
Rami Sabbagh <ramilego4game@gmail.com>
2016-08-01 11:27:48 +0300

    WIP Slider

--- p-mug/actions/slideShape.lua
+++ p-mug/actions/slideShape.lua
@@ -4,9 +4,10 @@ local ABase = require(Path..".api.actionbase")
 
 local ADO = Class("action.dragObject",ABase)
 
-function ADO:initialize()
+function ADO:initialize(x,y,length,orientation,slideShape)
   ABase.initialize(self)
   self.obstructed = {}
+  self.sX, self.sY, self.sL, self.sO, self.sS = x or 0, y or 0, length or 100, orientation or "h", slideShape
 end
 
 function ADO:handlerDown(x,y,pressure,obj,shape,shapes,obstruct)
@@ -15,8 +16,17 @@ end
 
 function ADO:handlerDragged(x,y,dx,dy,obj,shape,shapes,obstruct)
   if self.obstructed[obj] then return end
-  local oldx, oldy = obj:getPosition()
-  obj:setPosition(oldx+dx, oldy+dy)
+  if self.sS and not shape == self.sS then return end
+  local oldx, oldy = shape:getProperties()
+  if self.sO == "h" then
+    local newx, newy = oldx+dx, self.sY
+    if newx < self.sX then newx = self.sX elseif newx > self.sX+self.sL then newx = self.sX+self.sL end
+    shape:setProperties(newx,newy)
+  elseif self.sO == "v" then
+    local newx, newy = self.sX, oldy+dy
+    if newy < self.sY then newy = self.sY elseif newy > self.sY+self.sL then newy = self.sY+self.sL end
+    shape:setProperties(newx,newy)
+  end
 end
 
 function ADO:handlerReleased(x,y,pressure,obj,shape,shapes,obstruct)
--- p-mug/objects/Material/Slider.lua
+++ p-mug/objects/Material/Slider.lua
@@ -2,14 +2,33 @@ local Path = string.sub(..., 1, -string.len(".objects.Material.Slider"))
 local Class = require(Path..".third-party.middleclass")
 local OBase = require(Path..".api.objectbase")
 
+local Material = require(Path..".third-party.material-love")
+
 local OSlider = Class("object.slider",OBase)
 
-function OSlider:initialize(x,y,length,design,orientation,min,max)
+function OSlider:initialize(x,y,length,orientation,min,max)
   OBase.initialize(self)
   local PMug = require(Path)
 
+  --Shapes--
+  self.SCircle = PMug.Shape.circle(0,0,10):setDrawingArgs(true,true,{Material.colors("red","600")},{Material.colors.main("red")},{Material.colors("red","400")})
+
   --Actions--
-  self.DragAction = PMug.Action.dragObject()
+  self.slideAction = PMug.Action.slideShape(0,0,length,orientation,self.SCircle)
+
+  --Drawers--
+  self.Drawer = PMug.Drawer.material()
+
+  --Handlers--
+  self.Handler = PMug.Handler()
+
+  --Registering--
+  self:addHandler(self.Handler):addDrawer(self.Drawer)
+
+  self.Handler:linkShape(self.SCircle):addAction(self.slideAction)
+  self.Drawer:linkShape(self.SCircle)
+
+  self:registerShape(self.SCircle)
 
   self:setPosition(x,y)
 end
